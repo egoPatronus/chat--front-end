453c9c679e527ed40b685577cd9e90b3b42eb47e
Guilherme L. Moraes <guilherme.lmoraes32@gmail.com>
2021-04-19 18:01:48 -0300

    chore(repo): uploading changes

---
+++ src/components/main-menu/main-menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './main-menu.module.css';
+
+export default function MainMenu() {
+  return (
+    <section className={styles['main-menu__container']}>
+      main-menu
+    </section>
+  );
+}
--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,9 +1,31 @@
-import React from 'react';
+/* eslint-disable dot-notation */
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+import MainMenu from '../../components/main-menu/main-menu';
+import styles from './chat.module.css';
 
 export default function Chat() {
+  useEffect(() => {
+    function initializeWebSockets() {
+      const token = sessionStorage.getItem('s_id') ?? '';
+      const socket = io('http://localhost:8000', { auth: { token } });
+      socket.on('connect', () => {
+        console.log(socket);
+      });
+      socket.on('connect_error', (err) => {
+        console.log(err);
+      });
+    }
+
+    initializeWebSockets();
+  }, []);
+
   return (
-    <div>
-      teste
+    <div className={styles['page__body']}>
+      <section className={styles['chat__container']}>
+        <MainMenu />
+      </section>
     </div>
   );
 }
