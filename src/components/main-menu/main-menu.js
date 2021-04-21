08fd9ab069a4d4b4cd92312a6f2f3bf4ac3fb411
Guilherme L. Moraes <guilherme.lmoraes32@gmail.com>
2021-04-21 01:01:02 -0300

    chore(repo): uploading changes

--- src/components/main-menu/main-menu.js
+++ src/components/main-menu/main-menu.js
@@ -1,10 +1,53 @@
-import React from 'react';
+/* eslint-disable dot-notation */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+
 import styles from './main-menu.module.css';
 
-export default function MainMenu() {
+export default function MainMenu({ profile }) {
+  const [dropdown, setDropdown] = useState(false);
+  const { username } = profile ?? '';
+
+  const toggle = () => setDropdown(!dropdown);
+
   return (
     <section className={styles['main-menu__container']}>
-      main-menu
+      <header className={styles['main-menu__header']}>
+        <h1 className={styles['header__username']}>
+          {username}
+        </h1>
+        <ButtonDropdown isOpen={dropdown} toggle={toggle} className={styles['header__dropdown']}>
+          <DropdownToggle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="#919191"
+              className="bi bi-three-dots-vertical"
+              viewBox="0 0 16 16"
+            >
+              <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+            </svg>
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>
+              Logout
+            </DropdownItem>
+          </DropdownMenu>
+        </ButtonDropdown>
+      </header>
     </section>
   );
 }
+
+MainMenu.propTypes = {
+  profile: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,20 +1,28 @@
 /* eslint-disable dot-notation */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
+import { toast } from 'react-toastify';
 
 import MainMenu from '../../components/main-menu/main-menu';
 import styles from './chat.module.css';
 
 export default function Chat() {
+  const [profile, setProfile] = useState({
+    username: '',
+  });
+
   useEffect(() => {
     function initializeWebSockets() {
       const token = sessionStorage.getItem('s_id') ?? '';
       const socket = io('http://localhost:8000', { auth: { token } });
       socket.on('connect', () => {
-        console.log(socket);
+        socket.on('user:get-profile', (profileData) => setProfile(profileData));
       });
-      socket.on('connect_error', (err) => {
-        console.log(err);
+      socket.on('connect_error', (error) => {
+        toast.error(error, {
+          position: 'top-right',
+          autoClose: 5000,
+        });
       });
     }
 
@@ -24,7 +32,7 @@ export default function Chat() {
   return (
     <div className={styles['page__body']}>
       <section className={styles['chat__container']}>
-        <MainMenu />
+        <MainMenu profile={profile} />
       </section>
     </div>
   );
